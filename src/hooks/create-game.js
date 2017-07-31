32b66a1a582257e5a6e0c5e65c408cc99e2e8145
supermay <wu_jiamei@outlook.com>
2017-07-31 23:08:51 +0200

    Create Game Hook

--- src/hooks/create-game.js
+++ src/hooks/create-game.js
@@ -3,8 +3,31 @@
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function createGame (hook) {
-    // Hooks can either return nothing or a promise
-    // that resolves with the `hook` object for asynchronous operations
+
+    const { user } = hook.params;
+
+    // assign the owner of the game
+    hook.data.userId = user._id,
+    // add the owner to the players, as the first player in the game
+    hook.data.players = [{
+      userId: user._id,
+      pairs: []
+    }];
+
+    // Create a 15 * 15 empty board
+
+    const board = [];
+
+    for(let x = 1; x <= 15; x++) {
+      for(let y = 1; y <= 15; y++) {
+        board.push({
+          x: x,
+          y: y,
+          occupied: ''
+        });
+      }
+    }
+
     return Promise.resolve(hook);
   };
 };
